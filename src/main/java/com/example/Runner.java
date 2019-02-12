8b6e3182093040e2494ba32ddf253af8c388fa5d
Sebastian Otaegui <feniix@gmail.com>
2019-02-12 19:36:47 -0300

    initial commit

---
+++ src/main/java/com/example/Runner.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.example.Greeting;
+
+public class Runner {
+  public static void main(String[] args) {
+    Greeting greet = new Greeting();
+    greet.Greeting();
+  }
+}
